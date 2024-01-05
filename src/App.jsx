961648e649fff33857f79ee836d4be066e2d6a27
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2024-01-05 18:13:23 -0300

    Ejercicio realizado, faltan detalles del localstorage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import CardColors from "./components/CardColors";
+
+function App() {
+  return (
+    <>
+      <div className="min-vh-100 min-vh-100 py-4">
+        <CardColors />
+      </div>
+    </>
+  );
+}
+
+export default App;
