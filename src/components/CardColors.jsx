91c46eaf5f7ae552d1604ac4cf43f053fb063b51
Leonel Cordero <leonelrcordero22@gmail.com>
2024-02-01 20:13:49 -0300

    Logica de borrado implementada y README realizado

--- src/components/CardColors.jsx
+++ src/components/CardColors.jsx
@@ -1,80 +1,85 @@
-import React, { useState, useRef, useEffect } from 'react';
-import '../styles/cardColors.css';
-import { Button, Form } from 'react-bootstrap';
-import CardSmall from './CardSmall';
-import { Toast } from 'primereact/toast';
+import React, { useState, useRef, useEffect } from "react";
+import "../styles/cardColors.css";
+import { Button, Form } from "react-bootstrap";
+import CardSmall from "./CardSmall";
+import { Toast } from "primereact/toast";
 
 const CardColors = () => {
   const toast = useRef(null);
   const showRules = () => {
     toast.current.show({
-      severity: 'warn',
-      summary: 'Reglas',
-      detail: 'Puede ingresar colores fijos o hexadecimales.',
+      severity: "warn",
+      summary: "Reglas",
+      detail: "Puede ingresar colores fijos o hexadecimales.",
       life: 3000,
     });
   };
   const showSucces = () => {
     toast.current.show({
-      severity: 'success',
-      summary: 'Color agregado',
-      detail: 'Color agregado correctamente.',
+      severity: "success",
+      summary: "Color agregado",
+      detail: "Color agregado correctamente.",
       life: 3000,
     });
   };
-  const [color, setColor] = useState('white');
+  const [color, setColor] = useState("white");
+  const [inputColor, setInputColor] = useState("");
   const [colorSave, setColorSave] = useState(
-    JSON.parse(localStorage.getItem('colorKey')) || []
+    JSON.parse(localStorage.getItem("colorKey")) || []
   );
-  const onEnterPress = (event) => {
-    console.log(event);
-    if (event.key == 'Enter') {
-      if (event.target.value.trim().length > 0) {
-        let arrayAux = [...colorSave];
-        setColor(event.target.value);
-        let auxEncontrado = arrayAux.filter((elm) => elm == event.target.value);
-        if (auxEncontrado.length === 0) {
-          arrayAux.push(event.target.value.trim());
-          showSucces();
-        } else {
-          alert('color repetido');
-        }
-        setColorSave(arrayAux);
+  const onEnterPress = () => {
+    if (inputColor.trim().length > 0) {
+      let arrayAux = [...colorSave];
+      setColor(inputColor);
+      let auxEncontrado = arrayAux.filter((elm) => elm == inputColor);
+      if (auxEncontrado.length === 0) {
+        arrayAux.push(inputColor.trim());
+        showSucces();
       } else {
-        showRules();
+        alert("color repetido");
       }
+      setColorSave(arrayAux);
+    } else {
+      showRules();
     }
   };
   useEffect(() => {
-    localStorage.setItem('colorKey', JSON.stringify(colorSave));
+    localStorage.setItem("colorKey", JSON.stringify(colorSave));
   }, [colorSave]);
+  const deleteColor = (nombre) => {
+    let aux = [...colorSave].filter((elm) => elm !== nombre);
+    setColorSave(aux);
+  };
   return (
-    <div className='container'>
-      <div className='principalContainer p-0 py-3'>
-        <h3 className='px-4 m-0 mb-3'>Administrar colores</h3>
+    <div className="container">
+      <div className="principalContainer p-0 py-3">
+        <h3 className="px-4 m-0 mb-3">Administrar colores</h3>
         <Toast ref={toast} />
-        <div className='infoContainer py-4 d-flex align-items-center justify-content-center'>
-          <div className='colorBlock' style={{ backgroundColor: color }}></div>
-          <Form.Group className='mx-5'>
+        <div className="infoContainer py-4 d-flex align-items-center justify-content-center">
+          <div className="colorBlock" style={{ backgroundColor: color }}></div>
+          <Form.Group className="mx-5">
             <Form.Control
-              type='text'
-              placeholder='Ingrese un color'
-              onKeyDown={onEnterPress}
+              type="text"
+              placeholder="Ingrese un color"
+              onChange={(event) => setInputColor(event.target.value)}
             />
+            <div className="d-flex justify-content-end">
+              <Button
+                className="mt-3 me-3"
+                variant="primary"
+                type="button"
+                onClick={onEnterPress}
+              >
+                Guardar
+              </Button>
+            </div>
           </Form.Group>
         </div>
-        <div className='d-flex justify-content-end'>
-          <Button
-            className='mt-3 me-3'
-            variant='primary'
-            onClick={onEnterPress}
-          >
-            Guardar
-          </Button>
-        </div>
       </div>
-      <div className='row m-0'>
-        <CardSmall />
+      <div className="row m-0">
+        {colorSave.map((elm, id) => (
+          <CardSmall colorNombre={elm} key={id} deleteColor={deleteColor}/>
+        ))}
       </div>
     </div>
   );
--- src/components/CardSmall.jsx
+++ src/components/CardSmall.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import "../styles/CardSmall.css";
 
-const CardSmall = () => {
+const CardSmall = ({ colorNombre, deleteColor }) => {
   return (
     <div className="col-lg-4 col-md-6 p-0">
       <div className="cardSmallContainer m-4 py-2">
-        <h4 className="px-4 m-0 mb-3">Nombre color</h4>
+        <h4 className="px-4 m-0 mb-3">{colorNombre}</h4>
         <div className="d-flex justify-content-center">
-          <div className="colorBlock"></div>
+          <div
+            className="colorBlock"
+            style={{ backgroundColor: colorNombre }}
+          ></div>
         </div>
         <div className="d-flex justify-content-end">
-          <Button className="mt-3 me-3" variant="danger">
+          <Button
+            className="mt-3 me-3"
+            variant="danger"
+            onClick={() => deleteColor(colorNombre)}
+          >
             Borrar
           </Button>
         </div>
